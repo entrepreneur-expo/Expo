c3f3b141f88bffbc2b34a586a5e152f387724b0b
Chris Farley <cfarley95@gmail.com>
2019-06-26 00:05:50 -0400

    Added product class

---
+++ Expo/objects/product.swift
@@ -0,0 +1,36 @@
+//
+//  product.swift
+//  Expo
+//
+//  Created by Chris Farley on 6/26/19.
+//  Copyright © 2019 ADNAP. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+class product{
+    var name: String?
+    var productLink: String?
+    var productPrice: String?
+    var productImageURL: String?
+    var productDescription: String?
+    
+    
+    
+    
+    
+    
+    
+    
+    func dictionaryToClass(dictionary:NSDictionary){
+        let dict = JSON(dictionary)
+        self.name = dict["name"].stringValue
+        self.productImageURL = dict["productimageurl"].stringValue
+        self.productDescription = dict["productDescription"].stringValue
+        self.productPrice = dict["productPrice"].stringValue
+        
+        self.productImageURL = dict["productimageurl"].stringValue
+        
+        
+    }
+}
